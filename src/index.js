f0fe2ab05aaa5a1d22bef439aad12fd14b3d16ea
jesseworking <1222jesse@gmail.com>
2022-08-04 03:43:08 -0700

    test performance tech

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+import VirtualizedList from './VirtualizedList';
+import Counter from './Counter';
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
